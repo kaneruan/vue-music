751215a054a2dbba34841de040827b693a688d7a
rhb <1041763635@qq.com>
2017-07-06 15:30:44 +0800

    add momentum to control

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -23,6 +23,10 @@
       listenScroll: {
         type: Boolean,
         default:false
+      },
+      momentum: {
+        type: Boolean,
+        default:true
       }
     },
     mounted() {
@@ -38,7 +42,8 @@
         }
         this.scroll = new BScroll(this.$refs.wrapper, {
           probeType: this.probeType,
-          click: this.click
+          click: this.click,
+          momentum: this.momentum
         })
         this.scroll.on('touchend', (pos) => {
           if (pos.y > 50) {
